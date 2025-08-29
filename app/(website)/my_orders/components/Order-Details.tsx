2766ed48152acb736fcae6b5512fc9ddbb0ce1ef
Apson-Sapkota <tiger.white10a@gmail.com>
2025-08-29 15:13:05 +0545

    UPDATE: Order Details Page Updated

--- app/(website)/my_orders/components/Order-Details.tsx
+++ app/(website)/my_orders/components/Order-Details.tsx
@@ -26,10 +26,10 @@ const OrderProductDetails: React.FunctionComponent<OrderProductProps> = ({ Order
     return (
         <div>
             {OrderDetails.map((item, index) => (
-                <div className="relative flex flex-row items-start gap-2 p-2 md:gap-4 md:p-4 bg-white top-2" key={index}>
+                <div className="relative flex flex-row items-start gap-2 p-2 bg-white md:gap-4 md:p-4 top-2" key={index}>
                     {/* Product Image */}
 
-                    <div className="relative flex-shrink-0 w-24 h-24 md:w-32 md:h-28 rounded-lg overflow-hidden">
+                    <div className="relative flex-shrink-0 w-24 h-24 overflow-hidden rounded-lg md:w-32 md:h-28">
                         <Image
                             src={item.product.image}
                             alt={item.product.name}
@@ -38,39 +38,48 @@ const OrderProductDetails: React.FunctionComponent<OrderProductProps> = ({ Order
                             className="object-cover w-full h-full"
                         />
                     </div>
-                    <div className="flex flex-col xl:flex-row gap-2 w-full md:justify-between">
+                    <div className="flex flex-col w-full gap-2 md:justify-between">
                         <div className="space-y-2">
                             {/* Boxes */}
-                            <div className=" flex justify-between gap-3 md:gap-9">
-                                <div className="border-[1px] border-gray-500 px-1 py-1  md:font-semibold font-poppins text-xs md:text-sm">
-                                    Date:{item.product.created_at}
+                            <div className="flex flex-wrap gap-3 md:gap-9">
+                                <div className="border-[1px] border-gray-500 px-1 lg:min-w-70 py-1  md:font-semibold font-poppins text-xs md:text-sm">
+                                    Date: {new Date(item.product.created_at).toLocaleString()}
                                 </div>
                                 <div
-                                    className="border-[1px] border-gray-500 px-1 py-1  md:font-semibold font-poppins text-xs md:text-sm">
+                                    className="border-[1px] border-gray-500 px-1 py-1 lg:min-w-30  md:font-semibold font-poppins text-xs md:text-sm">
                                     Quantity: {item.product.quantity}
                                 </div>
-                                <div className="border-[1px] border-gray-500 px-1 py-1 md:font-semibold font-poppins text-xs md:text-sm">
+                                <div className="border-[1px] border-gray-500 px-1 py-1 lg:min-w-30 md:font-semibold font-poppins text-xs md:text-sm">
+                                    Nrs {item.product.price}
+                                </div>
+                                <div className="border-[1px] border-gray-500 px-1 py-1 lg:min-w-30 md:font-semibold font-poppins text-xs md:text-sm">
+                                    Nrs {item.product.price}
+                                </div>
+                                <div className="border-[1px] border-gray-500 px-1 py-1 lg:min-w-30 md:font-semibold font-poppins text-xs md:text-sm">
+                                    Nrs {item.product.price}
+                                </div>
+                                <div className="border-[1px] border-gray-500 px-1 py-1 lg:min-w-30 md:font-semibold font-poppins text-xs md:text-sm">
                                     Nrs {item.product.price}
                                 </div>
                             </div>
                             {/* Title Description */}
                             <div className="flex flex-col max-w-[60vw]">
-                                <h3 className="font-bold font-playfair text-sm md:text-xl xl:text-2xl text-foreground leading-snug line-clamp-2">
+                                <h3 className="text-sm font-bold leading-snug font-playfair md:text-xl xl:text-2xl text-foreground line-clamp-2">
                                     {item.product.name}
                                 </h3>
                                 <h3
-                                    className="text-xs md:text-sm text-foreground leading-snug truncate line-clamp-2"
+                                    className="text-xs leading-snug md:text-sm text-foreground line-clamp-2"
                                     dangerouslySetInnerHTML={{
-                                        __html: DOMPurify.sanitize(item.product.detail_description || "")
+                                        __html: DOMPurify.sanitize(item.product.general_description || "")
                                     }}
                                 />
                             </div>
                         </div>
 
                         {/* Review Return */}
-                        <div className="flex  md:items-center md:gap-3 justify-end xl:justify-between w-full">
+                        <div className="flex justify-end w-full md:items-center md:gap-3 xl:justify-between">
                             {OrderStatus?.name.toLowerCase() === "delivered" ? (
-                                <div className="flex justify-end gap-5 md:items-start items-center md:flex-row  xl:flex-row xl:justify-end w-full">
+                                <div className="flex items-center justify-end w-full gap-5 md:items-start md:flex-row xl:flex-row xl:justify-end">
                                     <button className="bg-secondary border cursor-pointer border-primary rounded-none hover:bg-primary hover:text-white text-black px-1 py-1 md:px-1 md:py-1 xl:px-2 xl:py-2 text-[12px] md:text-sm whitespace-nowrap">
                                         Write Review
                                     </button>
@@ -81,15 +90,31 @@ const OrderProductDetails: React.FunctionComponent<OrderProductProps> = ({ Order
                                         Return Order
                                     </button>
                                 </div>
-                            ) : (
+                            ) : OrderStatus?.name.toLowerCase() === "cancelled" ? (
                                 <button
                                     onClick={handleCancelOrder}
-                                    className="flex justify-end items-center gap-2 w-full cursor-pointer md:items-center text-primary hover:text-primary text-xs md:text-sm font-medium"
+                                    className="flex items-center justify-end w-full gap-2 text-xs font-medium cursor-pointer text-primary md:items-center hover:text-primary md:text-sm"
+                                >
+                                    Cancelled
+
+                                </button>
+                            ) : OrderStatus?.name.toLowerCase() === "pending" ? (
+                                <button
+                                    onClick={handleCancelOrder}
+                                    className="flex items-center justify-end w-full gap-2 text-xs font-medium cursor-pointer md:items-center text-primary hover:text-primary md:text-sm"
                                 >
                                     Cancel Order
                                     <CircleAlert className="w-4 h-4" />
                                 </button>
-                            )}
+                            ) : OrderStatus?.name.toLowerCase() === "returned" ? (
+                                <button
+                                    onClick={handleCancelOrder}
+                                    className="flex items-center justify-end w-full gap-2 text-xs font-medium text-yellow-600 cursor-pointer md:items-center hover:text-primary md:text-sm"
+                                >
+                                    Returned
+
+                                </button>
+                            ) : null}
                         </div>
                     </div>
                 </div>
--- app/(website)/my_orders/components/Order-Header.tsx
+++ app/(website)/my_orders/components/Order-Header.tsx
@@ -14,15 +14,15 @@ const OrderHeader: React.FunctionComponent<OrderHeaderDetails> = ({ orderItems }
     const { id, order_details, order_status } = orderItems ?? {};
 
     return (
-        <section>
+        <section className="shadow">
 
-            <div className="md:h-12 h-10 px-4 md:px-6 bg-secondary rounded-sm font-medium text-custom-black text-sm flex items-center justify-between">
+            <div className="flex items-center justify-between h-10 px-4 text-sm font-medium rounded-sm md:h-12 md:px-6 bg-secondary text-custom-black">
                 {/* Left side: Order Info */}
-                <div className="flex flex-wrap gap-2 md:gap-6 xl:gap-8 font-poppins text-xs md:text-sm">
+                <div className="flex flex-wrap gap-2 text-xs md:gap-6 xl:gap-8 font-poppins md:text-sm">
                     <h4>Order No:{id}</h4>
                 </div>
                 {/* Right side: Status */}
-                <div className="flex flex-wrap gap-2 items-center md:gap-6 text-xs md:text-sm">
+                <div className="flex flex-wrap items-center gap-2 text-xs md:gap-6 md:text-sm">
                     <h4>
                         Total Items: {order_details?.length}
                     </h4>
