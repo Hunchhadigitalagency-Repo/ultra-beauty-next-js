50eca5200357024552f5d2ef2976c5c0b82f86cc
Sugambaj7 <sugambaj123@gmail.com>
2025-08-31 17:40:19 +0545

    UPDATE: Cancel Order Form &  Order Action Card & Dynamic Cancel Order Page Updated

--- schemas/cancel-order/cancel_order.ts
+++ schemas/cancel-order/cancel_order.ts
@@ -2,8 +2,7 @@ import * as z from 'zod'
 
 export const CancelOrderSchema = z.object({
     reason: z.string().min(1, { message: "Reason is required" }),
-    field: z.string().optional()
-
+    additional_info: z.string().optional()
 });
 
 export type CancelFormValues = z.infer<typeof CancelOrderSchema>;
