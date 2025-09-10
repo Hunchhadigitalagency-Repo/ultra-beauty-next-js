4cd20f8fdba1232f239e5c5e227ed6de60e3dbcb
Vneet Karki <vneetkarki@Vneets-MacBook-Air.local>
2025-09-10 11:42:04 +0545

    ready to deploy

--- ecosystem_staging.config.js
+++ ecosystem_staging.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     apps: [
         {
-            name: 'basera-frontend-staging',
+            name: 'ultra-beauty-frontend-staging',
             script: 'npm',
             args: 'start',
-            cwd: '/mnt/volume_blr1_02/projects/basera-next-js-staging',
+            cwd: '/var/www/staging/ultra-beauty-next-js',
             instances: 1,
             exec_mode: 'fork',
             env: {
