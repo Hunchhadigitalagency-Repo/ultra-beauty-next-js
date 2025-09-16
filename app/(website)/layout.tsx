60933a951a693c2f1c573250de5cdb3265cae140
Sugambaj7 <sugambaj123@gmail.com>
2025-09-16 16:27:53 +0545

    UPDATE: Search Bar Updated

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -4,7 +4,7 @@ import type React from "react";
 import Navbar from "@/components/common/navbar/navbar";
 import Footer from "@/components/common/footer/footer";
 import MobileFootbar from "@/components/mobile/MobileFootbar";
-import MobileFilterMenu from "@/components/mobile/MobileFilterMenu";
+// import MobileFilterMenu from "@/components/mobile/MobileFilterMenu";
 import AnnouncementBar from "@/components/common/navbar/announcement-bar";
 
 const WebsiteLayout = ({ children }: { children: React.ReactNode }) => {
@@ -13,7 +13,7 @@ const WebsiteLayout = ({ children }: { children: React.ReactNode }) => {
     <div className="bg-white">
       <AnnouncementBar />
       <Navbar />
-      <MobileFilterMenu />
+      {/* <MobileFilterMenu /> */}
       {children}
       <MobileFootbar />
       <Footer />
--- components/common/navbar/search-modal.tsx
+++ components/common/navbar/search-modal.tsx
@@ -1,15 +1,17 @@
 "use client";
+
 import Image from "next/image";
-import React, { useState } from "react";
 import { Result } from "@/types/product";
 import { ArrowUpLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import SearchBox from "../filter/search-box";
 import { useAppSelector } from "@/redux/hooks";
+import { Skeleton } from "@/components/ui/skeleton";
+import React, { useCallback, useState } from "react";
 import { useInfiniteFetch } from "@/hooks/use-infinite-fetch";
 
 interface SearchModalprops {
-    onClose: () => void;
+    onClose?: () => void;
 }
 
 const SearchModal: React.FunctionComponent<SearchModalprops> = ({ onClose }) => {
@@ -27,37 +29,48 @@ const SearchModal: React.FunctionComponent<SearchModalprops> = ({ onClose }) => 
     if (searchTerm) {
         queryParams.append("search", searchTerm);
     }
-
     const path = `public-products${queryParams.toString() ? `?${queryParams.toString()}` : ""}`;
 
-    const handleSearchValue = (value: string) => {
+    const handleSearchValue = useCallback((value: string) => {
         setSearchTerm(value);
-    };
+    }, []);
 
     const {
-        data: products,
+        data: products, loading
     } = useInfiniteFetch<Result>(path || "", "", searchTerm.toString(), "", isLoggedIn);
 
-    return (
-        <div className="absolute right-0 z-50 w-full mt-0 bg-white shadow-md top-full padding-y lg:mt-4">
-            <div className="flex flex-col items-center w-full gap-4">
-                {/* Search Box */}
-                <SearchBox
-                    placeholder="Find the Product of Your Choice"
-                    searchBoxClassName='w-96 md:w-[500px] xl:w-[800px]'
-                    sendValue={handleSearchValue}
-                />
 
-                {/* Search Results */}
-                <div className="w-96 md:w-[500px] xl:w-[800px] bg-white max-h-[400px] overflow-y-auto">
-                    {searchTerm.length > 0 && (
+    return (
+        <div className="flex flex-col items-center w-full gap-2 ">
+            {/* Search Box */}
+            <SearchBox
+                placeholder="Find the Product of Your Choice"
+                sendValue={handleSearchValue}
+            />
+
+            {/* Search Results */}
+            <div className="w-full bg-white rounded-md">
+                {loading ?
+                    <ul className="rounded-md border space-y-2 overflow-y-auto divide-y divide-gray-100 h-[400px] lg:h-[350px] xl:h-[450px] p-2">
+                        {Array.from({ length: 6 }).map((_, i) => (
+                            <li
+                                key={i}
+                                className="flex items-center justify-between px-2 py-3 border-gray-200"
+                            >
+                                <div className="flex items-center w-full gap-2 md:gap-3">
+                                    <Skeleton className="w-12 h-12 rounded md:w-16 md:h-16" />
+                                    <Skeleton className="w-full h-12 rounded md:h-16" />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                    : searchTerm.length > 0 && (
                         products.length > 0 ? (
-                            <ul className="space-y-2">
+                            <ul className="rounded-md border space-y-2 overflow-y-auto divide-y divide-gray-100 h-[400px] lg:h-[350px] xl:h-[450px]">
                                 {products.map((item, index) => (
                                     <li
                                         key={index}
-
-                                        className="flex items-center justify-between px-2 py-3 rounded-md cursor-pointer select-none focus:bg-pink-100 hover:bg-white hover:shadow-md focus:outline-none"
+                                        className="flex items-center justify-between px-2 py-3 border-gray-200 rounded-md cursor-pointer hover:bg-gray-50"
                                     >
                                         <div className="flex items-center gap-1 md:gap-3">
                                             <div className="relative w-12 h-12 md:w-16 md:h-16">
@@ -72,21 +85,23 @@ const SearchModal: React.FunctionComponent<SearchModalprops> = ({ onClose }) => 
                                         </div>
                                         <ArrowUpLeft onClick={() => {
                                             router.push(`/shop/product/${item.slug_name}`);
-                                            onClose();
+                                            onClose?.();
                                         }}
                                             className="w-4 h-4 md:w-5 md:h-5 hover:shadow-md" />
                                     </li>
                                 ))}
                             </ul>
                         ) : (
-                            <p className="text-sm text-gray-500">
-                                No results found
-                            </p>
+                            <div className="flex justify-center items-center border rounded  h-[400px] lg:h-[350px] xl:h-[450px]">
+                                <p className="px-4 py-4 text-sm text-red md:text-base">
+                                    No results found !
+                                </p>
+                            </div>
                         )
-                    )}
-                </div>
+                    )
+                }
             </div>
-        </div >
+        </div>
     );
 };
 
