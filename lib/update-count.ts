c47b63bc37808395abb670a9953ee404459077b9
Ashik302 <ashikkoirala68@gmail.com>
2025-12-24 10:33:27 +0545

    Fixed: login issue

--- lib/update-count.ts
+++ lib/update-count.ts
@@ -1,19 +1,28 @@
-import { setCartCount,  } from "@/redux/features/cart-slice";
+import { setCartCount } from "@/redux/features/cart-slice";
 import { setNotificationCount } from "@/redux/features/wishList-slice";
+import { useAppSelector } from "@/redux/hooks";
 import { AppDispatch } from "@/redux/store";
-import api from "@/services/api-instance";
+import apiBase from "@/services/api-base-instance";
 
 export const updateCartAndWishlistCounts = async (dispatch: AppDispatch) => {
+  const { accessToken } = useAppSelector((state) => state.authentication);
   try {
     const [notificationRes, cartRes] = await Promise.all([
-      api.get("/cms/notifications-views/"),
-      api.get("/carts"),
+      apiBase.get("/cms/notifications-views/", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
+      apiBase.get("/carts", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }),
     ]);
 
     const unreadNotificationCount =
-      notificationRes?.data?.filter(
-        (not: any) => not.is_viewed === false
-      ).length || 0;
+      notificationRes?.data?.filter((not: any) => not.is_viewed === false)
+        .length || 0;
 
     const cartCount = cartRes?.data?.results?.length || 0;
 
